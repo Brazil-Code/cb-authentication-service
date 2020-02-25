0ab6e8bcbcd31b0c19dcf766cfaf083d35c5b5d8
Gabriel Oliveira <gabriel.oliveira@engdb.com.br>
2020-02-25 13:06:47 -0300

    Estrutura inicial

---
+++ src/test/java/br/com/brazilcode/cb/authentication/CbAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brazilcode.cb.authentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CbAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
